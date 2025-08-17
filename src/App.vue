ce07f228d3ddfe757a922628755bdd290c169d8e
Alexander Milev <alexandermilev94@abv.bg>
2025-08-17 12:26:00 +0300

    Import forecastDay component, change some styling, fetch both current weather and forecast in one promise and resolve them together for faster performance

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,44 @@
 	import { getWeather, getForecast } from "./api/weather";
 	import SearchBar from "./components/SearchBar.vue";
 	import CurrentWeather from "./components/CurrentWeather.vue";
+	import ForecastDay from "./components/ForecastDay.vue";
+	import { getForecastChunk } from "./api/utils";
 
+	/* Stores current weather data */
 	const weather = ref(null);
+	/* Stores error message */
 	const error = ref("");
+	/* Loading indicator for async calls */
 	const loading = ref(false);
+	/* Stores 5-day forecast data */
+	const forecast = ref([]);
+	
+	/* Determines whether to show the container with results/errors/loading spinner */
 	const showContainer = computed(() => weather.value || error.value || loading.value)
 
+
 	async function fetchWeather(city) {
+		/* Reset state before making new request */
 		error.value = "";
+		forecast.value = []
 		weather.value = null;
 		loading.value = true
 
 		try {
-			weather.value = await getWeather(city);
+			/* Fetch both current weather and forecast in parallel */
+			const [currentWeatherData, forecastData] = await Promise.all([
+				getWeather(city),
+				getForecast(city),
+			])
+
+			/* Store weather result in state */
+			weather.value = currentWeatherData
+			/* Extract daily forecast chunks */
+			forecast.value = getForecastChunk(forecastData.list)
 		} catch (err) {
+			/**
+			 * Handle different types of errors(API response, network, unknown)
+			 */
 			if (err.response) {
 				const messages = {
 					404: "City not found. Please check your spelling.",
@@ -29,6 +53,7 @@
 				error.value = "An unexpected error occurred.";
 			}
 		} finally {
+			/* Reset loading value regardless success/failure */
 			loading.value = false;
 		}
 	}
@@ -36,7 +61,7 @@
 
 <template>
 	<div class="min-h-screen flex justify-center bg-blue-400 p-4">
-		<div class="w-full max-w-md">
+		<div class="w-full">
 			<div class="p-6 bg-blue-800/30 backdrop-blur-md shadow-xl rounded-2xl">
 				<h1 class="text-3xl font-bold mb-6 text-center text-white drop-shadow">
 					Weather App
@@ -45,7 +70,7 @@
 				<SearchBar @search="fetchWeather" />
 			</div>
 
-			<div v-if="showContainer" class="mt-6 bg-blue-800/30 shadow-lg rounded-2xl p-6 text-center text-white">
+			<div v-if="showContainer" class="mt-6 p-6 bg-blue-800/30 backdrop-blur-md shadow-xl rounded-2xl">
 				<p v-if="error" class="text-white font-bold text-center bg-red-500/80 p-3 rounded-lg">
 					{{ error }}
 				</p>
@@ -56,12 +81,25 @@
 
 				<CurrentWeather
 					v-if="weather"
+					class="text-center text-white"
 					:city="weather.name"
 					:temp="weather.main.temp.toFixed()"
 					:feelsLike="weather.main.feels_like.toFixed()"
 					:description="weather.weather[0].main"
 					:icon="weather.weather[0].icon"
 				/>
+				
+				<div v-if="forecast.length" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-6">
+					<ForecastDay
+						v-for="day in forecast"
+						:key="day.date"
+						:day="day.weekday"
+						:date="day.fullDate"
+						:tempMax="day.tempMax"
+						:tempMin="day.tempMin"
+						:icon="day.icon"
+					/>
+				</div>
 			</div>
 		</div>
 	</div>
